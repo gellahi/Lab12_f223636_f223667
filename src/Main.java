31c4f4b81f0ec723064f4b8d4b4510929edafdb4
lab <lab@lab1-42.NUCFD.edu.local>
2024-11-14 11:39:04 +0500

    Fix critical bug in Main class

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to the application!");
         greetUser();
+        /// bug fix
     }
 
     public static void greetUser() {
