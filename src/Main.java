0bcfef01101ce5ee92d13cf9ceee500fa43665ea
lab <lab@lab1-42.NUCFD.edu.local>
2024-11-14 11:42:34 +0500

    Patner A

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,5 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Welcome to the application!");
-        greetUser();
-    }
-
-    public static void greetUser() {
-        System.out.println("Hello, User!");
-    }
+public static void main(String[] args) {
+System.out.println("Feature A work in progress.");
+}
 }
